cafb780de0a7b334ec9a1cfed3182deb18c37638
pnpsuM <89827636+pnpsuM@users.noreply.github.com>
2022-10-13 11:21:28 +0900

    Capsulized code and added image save function

--- SOH_func.py
+++ SOH_func.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as pl
 
 def get_data(NAME : str, drop_labels_x : list, drop_labels_y : list):
     data = pd.read_csv(NAME)
@@ -41,5 +42,18 @@ def split_data(x_data, y_data, num_batch):
     
     return x_train, y_train, x_test, y_test
 
-# def train_and_show(model, loss_dict, callback, data_x, data_y, epoch_index):
-    
+def show_and_proof(model, epoch_index, x_train, y_train, loss_dict, param):
+    # param = {'seq_len' : 25, 'sample_len' : 25, 'num_units' : 64, 'num_filters' : 64, 'window' : 3, 'drop_rate' : 0.2, 'num_epochs' : 800}
+    model.load_weights(f'Checkpoints\SOH_Checkpoint_{epoch_index}.h5')
+    loss = model.evaluate(x_train, y_train)
+    loss_dict[f'loss_{epoch_index}'] = loss
+    prediction = model.predict(x_train)
+    prediction = prediction.reshape(int(prediction.shape[0] * prediction.shape[1] * prediction.shape[2]), 1) 
+    y_graph = y_train.reshape(int(y_train.shape[0] * y_train.shape[1] * y_train.shape[2]), 1)
+    print(prediction.shape, y_graph.shape)
+    print(f'Estimation-{param["num_filters"]}FL-{param["num_units"]}UN-{epoch_index}EP-{param["seq_len"]}SQ')
+    pl.figure(dpi=150)
+    pl.plot(prediction)
+    pl.plot(y_graph)
+    pl.savefig(f'output\Estimation-{param["num_filters"]}FL-{param["num_units"]}UN-{epoch_index}EP-{param["seq_len"]}SQ.png')
+    pl.show()
